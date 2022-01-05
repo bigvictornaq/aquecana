97a24d9cd902a1b8360adbb66ac1fed5b777a88c
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2022-01-05 13:55:41 -0700

    se agrego footer

--- assets/administrador.php
+++ assets/administrador.php
@@ -211,8 +211,68 @@ $rows = $resultado->fetch_assoc();
   </section>
   <!-- Section: backgroud gris -->
 </main>
-<footer>
+<footer class="bg-primary text-white text-center text-lg-start">
+<!-- Grid container -->
+<div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Sistema Para la gestion de Expedientes</h5>
 
+        <p>
+          Es programa mamalon  par agestion de expedientes de los profesores etc etc etc.. blbla
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Info</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <p>Tecnológico Nacional de México
+                Campus Ciudad Juárez <br> Av.Tecnológico No.1340 C.P.32500 Ciudad Juárez, Chih.México</p>
+          </li>
+          <li>
+            <p>Departamento de sistemas <br>
+                (656) 688 - 2500 Ext.2301 </p>
+          </li>
+      
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="http://www.itcj.edu.mx/" class="text-white">ITCJ</a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/search/top?q=departamento%20de%20sistemas%20y%20computaci%C3%B3n%20tecnm%20campus%20cd.%20ju%C3%A1rez" class="text-white">DTPO</a>
+          </li>
+          <li>
+            <a href="https://github.com/bigvictornaq" class="text-white">NavaBoy</a>
+          </li>
+          
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">github.com/bigvictornaq</a>
+  </div>
+  <!-- Copyright -->
 </footer>
 </body>
 
