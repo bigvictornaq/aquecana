0fa920ccf075a9c7c6d990411b509c16e0b75d04
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2022-01-18 16:20:56 -0700

    fix error duplicade file in user add doc

--- assets/agregar_docs_user.php
+++ assets/agregar_docs_user.php
@@ -12,95 +12,120 @@ $resultado = $conexion->query($sql);
 $rows = $resultado->fetch_assoc();
 ?>
 
-<?php include 'includes/header.php'  ?>
-<!-- Section: backgroud gris align-items-center -->
-<section class="">
-      <div class="container">
+<?php include 'includes/header.php' ?>
+    <!-- Section: backgroud gris align-items-center -->
+    <section class="">
+        <div class="container">
             <div class="card w-100" style="width: 18rem;">
                 <div class="card-body">
-                     <h5 class="card-title">Lista de Usuarios</h5>
-                     <p class="card-text">
-                       Aqui se puede modificar todos los usuarios
-                     </p>
-                     <div class="d-flex justify-content-center">
-                         <!-- tablita con datitos -->
-                     
-                         <table id="tablita" class="display">
-                  <thead>
-                      <tr>
+                    <h5 class="card-title">Lista de Usuarios</h5>
+                    <p class="card-text">
+                        Aqui se puede modificar todos los usuarios
+                    </p>
+                    <div class="d-flex justify-content-center">
+                        <!-- tablita con datitos -->
+
+                        <table id="tablita" class="display">
+                            <thead>
+                            <tr>
                                 <th>Nombre Del Usuario</th>
                                 <th>Correo</th>
                                 <th>Documento Aiginado actualmente</th>
                                 <th>Agregar Document</th>
                                 <th>Ver perfil</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <?php 
-                      $sql = "SELECT * from usuarios";
-                      $result = mysqli_query($conexion, $sql);
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            $sql = "SELECT * from usuarios WHERE IdRol=2";
+                            $result = mysqli_query($conexion, $sql);
 
-                      while ($mostrar = mysqli_fetch_array($result)) {
-                      ?>
-                      <tr>
-                          <td><?=$mostrar['Nombre_Completo']?></td>
-                          <td><?=$mostrar['Correo']?></td>
-                          <td>
-                              <div class="row">
-                                  <form id="formdoc" action="php/add_doc_to_user.php?buscarId=<?=$mostrar['Id']?>" method="post">
-                                  <div class="col">
-                                      <select name="periodo" class="form-select" aria-label="Default select example=">
-                                          <option selected>Seleciona el documento</option>
+                            while ($mostrar = mysqli_fetch_array($result)) {
+                                ?>
+                                <tr>
+                                    <td><?= $mostrar['Nombre_Completo'] ?></td>
+                                    <td><?= $mostrar['Correo'] ?></td>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col">
+                                                <form
+                                                      action="php/add_doc_to_user.php?buscarId=<?=$mostrar['Id']?>"
+                                                      method="post">
+                                                    <select name="ped" class="form-control"
+                                                            aria-label="Default select example=">
+                                                        <option>Seleciona el documento</option>
+                                                        <?php
+                                                        $query = "SELECT documentos.idDoc, documentos.año, 
+                                                                    periodo.fecha_estipada FROM documentos 
+                                                                        INNER JOIN periodo ON documentos.idperiodo=periodo.id_periodo";
+                                                        $result2 = $conexion->query($query) or die($conexion->error);
 
-                                          <?php
-                                          $query = "SELECT documentos.idDoc, documentos.año, periodo.fecha_estipada FROM documentos INNER JOIN periodo ON documentos.idperiodo=periodo.id_periodo ";
-                                          $result2 = $conexion->query($query) or die($conexion->error);
+                                                        while ($show_docs = $result2->fetch_assoc()) {
+                                                            ?>
+                                                            <option value="<?= $show_docs['idDoc'] ?>">
+                                                                <?php echo $show_docs['fecha_estipada'] . "-" . $show_docs['año']; ?>
+                                                            </option>
+                                                        <?php } ?>
+                                                    </select>
+                                                    <button type="submit" value="ne" "
+                                                            class="btn btn-outline-secondary">
+                                                        Agregar Documento
+                                                    </button>
+                                                </form>
+                                            </div>
 
-                                          while($show_docs = $result2->fetch_assoc()){
-                                          ?>
-                                          <option value="<?= $show_docs['idDoc']?>"><?php echo $show_docs['fecha_estipada']."-".$show_docs['año'] ;?></option>
-                                          <?php  } ?>
-                                      </select>
-                                  </div>
-                                  <div class="col">
-                                      <button type="submit" value="ne" form="formdoc" class="btn btn-outline-secondary">
-                                          Agregar Documento
-                                      </button>
-                                  </div>
-                                  </form>
-                              </div>
-                          </td>
-                          <td>
-                              <a href="php/view_user_profile.php?buscarId=<?= $mostrar['Id'] ?>" class="btn btn-outline-info">Visulizar Perfil</a>
-
-                          </td>
-                      </tr>
-                      <?php }?>
-                  </tbody>
-                  <tfoot>
-                  <tr>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <a href="php/view_user_profile.php?buscarId=<?= $mostrar['Id'] ?>"
+                                           class="btn btn-outline-info">Visulizar Perfil</a>
+                                    </td>
+                                    <td>hola</td>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                            <tfoot>
+                            <tr>
                                 <th>Nombre</th>
-
                                 <th>Correo</th>
-
-                                <th>Agregar Documento
-                                </th>
+                                <th>Agregar Documento</th>
                                 <th>Ver perfil</th>
-                      </tr>
-                  </tfoot>
-            </table>
-            <!-- tablita con datitos -->
-                     </div>
-             
-                   
-                     <a href="#" class="card-link">Card link</a>
-                     <a href="#" class="card-link">Another link</a>
+                            </tr>
+                            </tfoot>
+                        </table>
+                        <!-- tablita con datitos -->
+                    </div>
+                        <!--modal-->
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                   <form class="">
+
+                                   </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Understood</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        <!--modal-->
+
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
                 </div>
             </div>
-          
-      </div>
-  </section>
-  <!-- Section: backgroud gris -->
-</main>
 
-<?php include 'includes/footer.php'  ?>
+        </div>
+    </section>
+    <!-- Section: backgroud gris -->
+    </main>
+
+<?php include 'includes/footer.php' ?>
--- assets/php/add_doc_to_user.php
+++ assets/php/add_doc_to_user.php
@@ -3,15 +3,55 @@
     include 'conexion_be.php';
 
     session_start();
+    $correo = $_SESSION['usuario'];
+    if (!isset($_SESSION['usuario'])) {
+    header("Location: ../index2.php");
+    }
+
     $id_user = $_GET['buscarId'];
 
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
-            $id_documento = $_POST['periodo'];
-            $add_doc_user = $conexion->query("UPDATE usuarios SET IdDoc={$id_documento} WHERE Id={$id_user} ")
+            $id_documento = $_POST['ped'];
+            echo $id_user;
+
+            $doc_result = $conexion->query("SELECT * FROM documentos WHERE IdDOC={$id_documento}")
+                                            OR die(mysqli_error($conexion));
+            $doc_selected = $doc_result->fetch_assoc();
+
+
+            if (file_exists('../Documentos/'.$correo.'/'.$doc_selected['url'])){
+
+                            $user_rs = $conexion->query("SELECT Correo FROM usuarios WHERE Id={$id_user}")
                                                 or die(mysqli_error($conexion));
-            if ($add_doc_user){
-                header("Location: ../expen_cr.php");
-            }else{
-                echo "Error: efesota $add_doc_user";
+                            $email_user = $user_rs->fetch_assoc();
+
+                    if(file_exists('../Documentos/'.$email_user['Correo'])){
+                        copy('../Documentos/'.$correo.'/'.$doc_selected['url'],'../Documentos/'.$email_user['Correo'].'/'.$doc_selected['url']);
+                    }else{
+                        mkdir('../Documentos/'.$email_user['Correo']); //creamos el directorio del usuario
+
+                        copy('../Documentos/'.$correo.'/'.$doc_selected['url'],'../Documentos/'.$email_user['Correo'].'/'.$doc_selected['url']);
+                    }
+
+                $add_doc_user = $conexion->query("UPDATE usuarios SET IdDoc={$id_documento} WHERE Id={$id_user} ")
+                or die(mysqli_error($conexion));
+
+                if ($add_doc_user){
+                    echo  $id_user.'</br>';
+                    echo '../Documentos/'.$email_user['Correo'];
+                    // header("Location: ../expen_cr.php");
+                }else{
+                    echo "Error: efesota $add_doc_user";
+                }
             }
+
+
+
+
+
+
+
+
+
+
     }
